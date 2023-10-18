b6c862ede4c0e09d516e23e1fc3f634b54973855
Kwillon <grand1stoomb@gmail.com>
2023-10-18 19:05:26 +0300

    Добавлен модуль strings.py, удалён текс

--- first/nums.py
+++ first/nums.py
@@ -1,8 +1,8 @@
 def nums(a, b):
     return a + b
 
-
-text = "new text"
+def mult(a, b):
+    return a * b
 
 
 def minus(a, b):
@@ -12,3 +12,4 @@ def minus(a, b):
 if __name__ == '__main__':
     print(nums(2, 3))
     print(minus(5, 6))
+    print(mult(7, 9))
